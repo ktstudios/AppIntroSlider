9c3919e59d29bd428c6049aed641ab821c35cc21
ktstudios <admin@ktstudios.com>
2014-08-05 11:55:59 -0400

    Major code change

--- www/js/app.js
+++ www/js/app.js
@@ -1,37 +1,130 @@
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array or 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('myApp', ['ionic', 'ngRoute'])
+angular.module('ionicApp', ['ionic'])
 
-.config(function ($compileProvider){
-  // Needed for routing to work
-  $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
-})
+.config(function($stateProvider, $urlRouterProvider) {
 
-.config(function($routeProvider, $locationProvider) {
-
-  // Set up the initial routes that our app will respond to.
-  // These are then tied up to our nav router which animates and
-  // updates a navigation bar
-  $routeProvider.when('/', {
-    templateUrl: 'templates/intro.html',
+  $stateProvider
+  .state('intro', {
+    url: '/',
+    templateUrl: 'intro.html',
     controller: 'IntroCtrl'
-  });
-
-  // if the url matches something like /pet/2 then this route
-  // will fire off the PetCtrl controller (controllers.js)
-  $routeProvider.when('/main', {
-    templateUrl: 'templates/main.html',
+  })
+  .state('main', {
+    url: '/main',
+    templateUrl: 'main.html',
     controller: 'MainCtrl'
   });
 
-  // if none of the above routes are met, use this fallback
-  // which executes the 'AppCtrl' controller (controllers.js)
-  $routeProvider.otherwise({
-    redirectTo: '/'
-  });
+  $urlRouterProvider.otherwise("/");
 
+})
+
+.controller('IntroCtrl', function($scope, $state) {
+ 
+  // Called to navigate to the main app
+  var startApp = function() {
+    $state.go('main');
+
+    // Set a flag that we finished the tutorial
+    window.localStorage['didTutorial'] = true;
+  };
+
+  //No this is silly
+  // Check if the user already did the tutorial and skip it if so
+if(window.localStorage['didTutorial'] === "true") {
+    console.log('Skip intro');
+    startApp();
+  }
+  else{
+  setTimeout(function () {
+		navigator.splashscreen.hide();
+	}, 750);
+  }
+  
+
+  // Move to the next slide
+  $scope.next = function() {
+    $scope.$broadcast('slideBox.nextSlide');
+  };
+
+  // Our initial right buttons
+  var rightButtons = [
+    {
+      content: 'Next',
+      type: 'button-positive button-clear',
+      tap: function(e) {
+        // Go to the next slide on tap
+        $scope.next();
+      }
+    }
+  ];
+  
+  // Our initial left buttons
+  var leftButtons = [
+    {
+      content: 'Skip',
+      type: 'button-positive button-clear',
+      tap: function(e) {
+        // Start the app on tap
+        startApp();
+      }
+    }
+  ];
+
+  // Bind the left and right buttons to the scope
+  $scope.leftButtons = leftButtons;
+  $scope.rightButtons = rightButtons;
+
+
+  // Called each time the slide changes
+  $scope.slideChanged = function(index) {
+
+    // Check if we should update the left buttons
+    if(index > 0) {
+      // If this is not the first slide, give it a back button
+      $scope.leftButtons = [
+        {
+          content: 'Back',
+          type: 'button-positive button-clear',
+          tap: function(e) {
+            // Move to the previous slide
+            $scope.$broadcast('slideBox.prevSlide');
+          }
+        }
+      ];
+    } else {
+      // This is the first slide, use the default left buttons
+      $scope.leftButtons = leftButtons;
+    }
+    
+    // If this is the last slide, set the right button to
+    // move to the app
+    if(index == 2) {
+      $scope.rightButtons = [
+        {
+          content: 'Start using MyApp',
+          type: 'button-positive button-clear',
+          tap: function(e) {
+            startApp();
+          }
+        }
+      ];
+    } else {
+      // Otherwise, use the default buttons
+      $scope.rightButtons = rightButtons;
+    }
+  };
+})
+
+.controller('MainCtrl', function($scope, $state) {
+  console.log('MainCtrl');
+  
+  setTimeout(function () {
+		navigator.splashscreen.hide();
+	}, 750);
+  
+  
+  $scope.toIntro = function(){
+    window.localStorage['didTutorial'] = "false";
+    $state.go('intro');
+  }
 });
-
